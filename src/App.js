65a54afbf8ad722111f941bf1a13b6c4ffce86ee
Sumit <jagtasumit668@gmail.com>
2023-10-18 19:51:16 +0530

    small changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Images_Grid from "../src/components/Images_Grid"
-import Search_Unit from './components/Search_Unit';
+
 function App() {
   return (
     <div className="App">
-     <Search_Unit/>
+     
      <Images_Grid/>
     </div>
   );
--- src/components/Images_Grid.jsx
+++ src/components/Images_Grid.jsx
@@ -2,27 +2,11 @@ import { useEffect, useState } from "react";
 import { FcLike } from "react-icons/fc";
 import axios from "axios";
 
-// const products = [
-//   {
-//     id: 1,
-//     name: "Basic Tee",
-//     href: "#",
-//     imageSrc:
-//       "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
-//     imageAlt: "Front of men's Basic Tee in black.",
-//     price: "$35",
-//     color: "Black",
-//   },
-//   // More products...
-// ];
-
 export default function Images_Grid() {
   const [photos, setPhotos] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
-    const url =
-      "https://api.unsplash.com/photos/?client_id=pWy9EcgK0erjCG64djnEpvlZrookYrc2cx4blLRQAyY";
+    const url = `https://api.unsplash.com/photos/?client_id=pWy9EcgK0erjCG64djnEpvlZrookYrc2cx4blLRQAyY`;
 
     axios
       .get(url)
@@ -32,11 +16,7 @@ export default function Images_Grid() {
       .catch((error) => {
         console.log("error occure", error);
       });
-  }, [searchQuery]);
-  const handleSearch = () => {
-    setPhotos([]);
-    setSearchQuery(searchQuery);
-  };
+  }, []);
 
   return (
     <div className="bg-white">
@@ -44,23 +24,6 @@ export default function Images_Grid() {
         <h2 className="text-2xl font-bold tracking-tight text-gray-900">
           Photos Gallery
         </h2>
-        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          <div className="mt-6 flex">
-            <input
-              type="text"
-              placeholder="Search for images"
-              value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
-              className="p-2 border rounded-lg mr-4"
-            />
-            <button
-              onClick={handleSearch}
-              className="p-2 bg-blue-500 text-white rounded-lg"
-            >
-              Search
-            </button>
-          </div>
-        </div>
 
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {photos.map((photo) => (
