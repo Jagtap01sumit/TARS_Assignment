00163bd82087e83d684ffb9f5eb46adf3dc37689
Sumit <jagtasumit668@gmail.com>
2023-10-19 19:35:28 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-import Images_Grid from "../src/components/Images_Grid"
+import { createContext, useState } from "react";
+import Images from "./components/Images";
+import Jumbutron from "./components/Jumbutron";
+import SearchField from "./components/SearchField";
+import useAxios from "./hooks/useAxios";
+
+
+export const ImageContext = createContext();
 
 function App() {
+  const [searchImage, setSearchImage] = useState('');
+  const { response, isLoading, error, fetchData } = useAxios(`search/photos?page=1&query=cats&client_id=${process.env.REACT_APP_ACCESS_KEY}`);
+
+  const value = {
+    response,
+    isLoading,
+    error,
+    fetchData,
+    searchImage,
+    setSearchImage
+  }
+
   return (
-    <div className="App">
-     
-     <Images_Grid/>
-    </div>
+    <ImageContext.Provider value={value}>
+      <Jumbutron>
+        <SearchField />
+      </Jumbutron>
+      <Images />
+    </ImageContext.Provider>
   );
 }
 
